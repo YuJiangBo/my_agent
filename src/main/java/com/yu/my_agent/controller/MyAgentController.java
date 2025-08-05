42e1478837adb4bd2932f06b5adfc0287e5beef9
余江波 <yjb@wupodata.com>
2025-08-05 08:59:02 +0800

    agent ReAct手动输入决定工具执行(非流式)

--- src/main/java/com/yu/my_agent/controller/MyAgentController.java
+++ src/main/java/com/yu/my_agent/controller/MyAgentController.java
@@ -1,7 +1,9 @@
 package com.yu.my_agent.controller;
 
+import com.yu.my_agent.ai.agent.YuManus;
 import com.yu.my_agent.ai.app.MyAgentApp;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,12 @@ public class MyAgentController {
 
     @Resource
     private MyAgentApp agentApp;
+
+    @Autowired
+    private YuManus yuManus;
+
     /**
-     * 同步调用 AI 恋爱大师应用
+     * 同步调用 AI
      *
      * @param message
      * @param chatId
@@ -26,5 +32,16 @@ public class MyAgentController {
     @GetMapping("/chat/sync")
     public String doChatWithAgentApp(String message, String chatId) {
         return agentApp.doChat(message, chatId);
+    }
+
+    /**
+     * 同步调用 AI 人工确认
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/chat/ack")
+    public String doChatWithAck(String message) {
+        return yuManus.run(message);
     }
 }
